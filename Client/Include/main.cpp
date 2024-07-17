54473503f391c80b359df7bae95eff56e18b376e
ohdam <erfrr@naver.com>
2024-07-17 22:13:19 +0900

    [Modify] WinMain 명령행 인자로 ip주소 값을 받아오도록 구현

--- Client/Include/main.cpp
+++ Client/Include/main.cpp
@@ -1,10 +1,21 @@
 #include "pch.h"
 #include "Core.h"
-
+#include <sstream>
 
 int WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpszCmdParam, int nCmdShow)
 {
-	if (!Core::GetInst().Init(hInstance)) 
+	__int32 argc = 0;
+	string cmd_line(lpszCmdParam);
+	istringstream iss(cmd_line);
+	string arg;
+	vector<string> args;
+	while (iss >> arg)
+	{
+		args.push_back(arg);
+	}
+
+
+	if (!Core::GetInst().Init(hInstance, args[0].c_str()))
 	{
 		Core::GetInst().OnDestroy();
 		return -1;
